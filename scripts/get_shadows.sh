2fec653fd1cea056bbf12689f58a97cce78b1480
Matthew Titmus <mtitmus@flatiron.com>
2021-04-30 16:48:43 -0400

    Rm scripts

--- scripts/get_shadows.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-BACKEND_URL="http://localhost:27301"
-HEADERS=(-H "Content-Type: application/json")
-PID="Q_MATRIX"
-URL="$BACKEND_URL/api/1.0/signals/pid/$PID/shadows"
-
-curl "${HEADERS[@]}" -X GET $URL
